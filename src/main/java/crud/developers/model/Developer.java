c63d01163c76c06354caa809d30c3e0d1b012cd9
Sergiy <asd@sed.dfs>
2020-06-05 07:38:50 +0300

    Implements model layer

---
+++ src/main/java/crud/developers/model/Developer.java
@@ -0,0 +1,59 @@
+package crud.developers.model;
+
+import java.util.List;
+
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Specialty specialty;
+    private List<Skill> skills;
+
+    public Developer(Long id, String firstName, String lastName, Specialty specialty, List<Skill> skills) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialty = specialty;
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
