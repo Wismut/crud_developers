15d92449282c3f4a169dd6211b259f980051029a
Sergiy <asd@sed.dfs>
2020-06-14 17:34:26 +0300

    Remove query validator dependency

--- src/main/java/runner/Runner.java
+++ src/main/java/runner/Runner.java
@@ -1,10 +1,8 @@
 package runner;
 
 import liquibase.LiquibaseUtil;
-import org.hibernate.query.validator.CheckHQL;
 import view.impl.MainView;
 
-@CheckHQL
 public class Runner {
     public static void main(String[] args) {
         LiquibaseUtil.updateDB();
