8394e3dc0a5de5cb5be6b6fa80dec0974434793f
Sergiy <asd@sed.dfs>
2020-06-17 09:42:22 +0300

    Add servlets dependency

--- src/main/java/controller/SkillController.java
+++ src/main/java/controller/SkillController.java
@@ -3,14 +3,31 @@ package controller;
 import model.Skill;
 import service.SkillService;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-public class SkillController {
+@WebServlet(name = "SkillController", urlPatterns = "/skills")
+public class SkillController extends HttpServlet {
     private final SkillService skillService;
 
     public SkillController(SkillService skillService) {
         this.skillService = skillService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 
     public void deleteById(Long id) {
